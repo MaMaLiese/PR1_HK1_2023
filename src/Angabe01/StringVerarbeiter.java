327aea6d3c5d5ddf5644640d060ee9ed35e0e347
MaMaLiese <marlies.m@gmx.net>
2023-06-05 13:35:42 +0200

    Starter von HK1

---
+++ src/Angabe01/StringVerarbeiter.java
@@ -0,0 +1,18 @@
+package Angabe01;
+
+public class StringVerarbeiter {
+    public static void main(String[] args) {
+        String wort;
+        int laenge;
+
+
+        System.out.println("Dies und das");
+    }
+    public static lieferLaenge(String wort) {
+
+
+    }
+
+
+
+}
